b64af5adeb58735922c43ed6e270216cca2747ea
Kp Kumar <pkambalok@gmail.com>
2026-07-07 01:28:39 +0530

    changed the value storing logic

--- expense-tracker/stage_2.py
+++ expense-tracker/stage_2.py
@@ -6,10 +6,10 @@ while True:
         amount = float(input("Enter the expense amount: "))
         category = input("Enter expense the category: ")
         description = input("Enter the expense description: ")
-        expenses.append({"amount": f"{amount:.2f}", "category": f"{category}", "description": f"{description}"})
+        expenses.append({"amount": amount, "category": category, "description": description})
         print("Added the expense: ", expenses)
     elif menu == '2':
         for index, expense in enumerate(expenses):
-            print(f"{index+1}. Rs.{expense["amount"]} | {expense["category"]} | {expense["description"]}")
+            print(f"{index+1}. Rs.{expense["amount"]:.2f} | {expense["category"]} | {expense["description"]}")
     else:
         break
