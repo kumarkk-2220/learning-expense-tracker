83dae8345c7b7c8ae37054dbcf61d924a36634b3
Kp Kumar <pkambalok@gmail.com>
2026-07-16 01:45:51 +0530

    Descriptive functions

--- expense-tracker/stage_3.py
+++ expense-tracker/stage_3.py
@@ -1,40 +1,38 @@
-expenses = []
-
-def add_expenses():
-    amount = float(input("Enter the expense amount: "))
-    category = input("Enter the expense Category: ")
-    description = input("Enter the expense Description: ")
+def add_expenses(expenses: list, amount: float, category: str, description: str) -> dict:
     expense_dict = {"amount": amount, "category": category, "description": description}
     expenses.append(expense_dict)
-    print(expenses)
+    return expense_dict
 
-def list_expenses():
+def list_expenses(expenses: list) -> None:
     for index, expense in enumerate(expenses):
         print(f"{index+1}. Rs.{expense['amount']:.2f} | {expense['category']} | {expense['description']}")
 
-def category_totals():
+def category_totals(expenses: list) -> None:
     expense_summary = {}
     for expense in expenses:
         if expense['category'] not in expense_summary.keys():
-            print(f"{expense['category']} not found. hence added")
             expense_summary[expense['category']] = expense['amount']
         else:
             if expense['category'] in expense_summary.keys():
                 expense_summary[expense['category']] += expense['amount']
-    print(expense_summary)
+    return expense_summary
 
     
-def main():
+def main() -> None:
+    expenses_list = []
     while True:
         user_choice = input("Select an option: \n1. Add Expense\n2.List all expenses\n3.Exit\n4.Summary\nSelect a number from above: ")
         if user_choice == '1':
-            add_expenses()
+            amount = float(input("Enter the expense amount: "))
+            category = input("Enter the expense category: ").strip()
+            description = input("Enter the expense description: ").strip()
+            add_expenses(expenses_list, amount, category, description)
         elif user_choice == '2':
-            print(list_expenses())
+            list_expenses(expenses_list)
         elif user_choice == '3':
             break
         elif user_choice == '4':
-            category_totals()
+            print(category_totals(expenses_list))
         else:
             continue
 
